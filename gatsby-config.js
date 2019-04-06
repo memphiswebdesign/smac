5096ca91b6ddf583244ae66dfdfb0ca1704d3996
Adam Taylor <adamtaylor0795@gmail.com>
2019-04-06 13:04:32 -0500

    SAVEPOINT | Trying to understand how GraphQL works

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,15 @@ module.exports = {
         {
             resolve: `gatsby-source-filesystem`,
             options: {
+                name: 'blog',
                 path: `${__dirname}/src/blog`,
-                name: 'markdown-pages',
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: 'events',
+                path: `${__dirname}/src/events`,
             },
         },
         `gatsby-transformer-remark`,

e8545e75d75caf0a7445d7f48bb22352c1a83a6e
Adam Taylor <adamtaylor0795@gmail.com>
2019-04-06 17:21:32 -0500

    Removed the .cache folder which probably shouldn't be in git

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/adamtaylor/code/smac/node_modules/gatsby-plugin-netlify-cms/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/Users/adamtaylor/code/smac/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]

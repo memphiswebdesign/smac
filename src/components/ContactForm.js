235924e85fa415be52f766d69ac875ec89750a18
Adam Taylor <adamtaylor0795@gmail.com>
2019-04-08 20:33:46 -0500

    Added contact form and connected to netlify. Pushing up for testing.

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -6,47 +6,48 @@ export default () => (
             <div className="row">
                 <div className="divider-70 d-none d-xl-block"></div>
                 <div className="col-12">
-                    <h3 className="special-heading text-center">GET YOUR FREE PASS</h3>
+                    <h3 className="special-heading text-center">Contact Us</h3>
                     <div className="divider-60 d-none d-xl-block"></div>
                     <div className="divider-40 hidden-above-md"></div>
-                    <form action="#">
+
+                    <form action="/contact-success" name="contact-form" data-netlify-recaptcha="true" data-netlify="true">
                         <div className="row">
-                            <div className="col-xs-12 col-md-4">
+                            <div className="col-xl-12">
                                 <div className="form-group">
                                     <input className="form-control" type="text" name="text_1fcfb46"
                                            placeholder="Full Name" defaultValue="" required="required"/>
                                 </div>
                             </div>
-                            <div className="col-xs-12 col-md-4">
+                            <div className="col-xl-12">
                                 <div className="form-group">
                                     <input className="form-control" type="text" name="text_42598d5"
                                            placeholder="Phone Number" defaultValue="" required="required"/>
                                 </div>
                             </div>
-                            <div className="col-xs-12 col-md-2">
-                                <div className="form-group">
-                                    <div className="field-date">
-                                        <select className="form-control" name="date_time_42a77ed"
-                                                required="required">
-                                            <option value="">Date</option>
-                                            <option value="Date">Date</option>
-                                            <option value="Date">Date</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-xs-12 col-md-2">
-                                <div className="form-group">
-                                    <div className="field-date">
-                                        <select className="form-control" name="date_time_b63914d"
-                                                required="required">
-                                            <option value="">Time</option>
-                                            <option value="Time">Time</option>
-                                            <option value="Time">Time</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
+                            {/*<div className="col-xs-12 col-md-2">*/}
+                                {/*<div className="form-group">*/}
+                                    {/*<div className="field-date">*/}
+                                        {/*<select className="form-control" name="date_time_42a77ed"*/}
+                                                {/*required="required">*/}
+                                            {/*<option value="">Date</option>*/}
+                                            {/*<option value="Date">Date</option>*/}
+                                            {/*<option value="Date">Date</option>*/}
+                                        {/*</select>*/}
+                                    {/*</div>*/}
+                                {/*</div>*/}
+                            {/*</div>*/}
+                            {/*<div className="col-xs-12 col-md-2">*/}
+                                {/*<div className="form-group">*/}
+                                    {/*<div className="field-date">*/}
+                                        {/*<select className="form-control" name="date_time_b63914d"*/}
+                                                {/*required="required">*/}
+                                            {/*<option value="">Time</option>*/}
+                                            {/*<option value="Time">Time</option>*/}
+                                            {/*<option value="Time">Time</option>*/}
+                                        {/*</select>*/}
+                                    {/*</div>*/}
+                                {/*</div>*/}
+                            {/*</div>*/}
                         </div>
                         <div className="row">
                             <div className="col-12">
@@ -58,12 +59,13 @@ export default () => (
                         </div>
                         <div className="row">
                             <div className="col-12 text-center">
-                                <button className="btn btn-maincolor" type="submit">get pass now</button>
-                                <button className="btn btn-outline-maincolor" type="submit">our contacts
-                                </button>
+                                <button className="btn btn-maincolor" type="submit">Send Message</button>
+                                <div data-netlify-recaptcha="true"></div>
+                                {/*<button className="btn btn-outline-maincolor" type="submit">our contacts</button>*/}
                             </div>
                         </div>
                     </form>
+
                 </div>
                 <div className="divider-40 d-none d-xl-block"></div>
             </div>
---
+++ src/pages/contact-success.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => (
+    <section className="ds s-py-60 s-py-md-90 c-mb-20 c-gutter-20 form-section">
+        Successfully submitted the form!
+    </section>
+)

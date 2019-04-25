a98fbd0bbc9ad66d6eea75a5a30618d123f0d8c9
Eli Brumley <elibrumley@gmail.com>
2019-04-25 09:38:40 -0500

    contact success animation fix

--- src/pages/contact-success.js
+++ src/pages/contact-success.js
@@ -9,7 +9,7 @@ export default () => (
 	    <section className="ds s-py-60 s-py-md-60 c-mb-20 c-gutter-20 form-section">
 	        <div className="container">
 	            <div className="row">
-	                <div className="col-md-12 text-center animate" data-animation="floating" data-delay="70">
+	                <div className="col-md-12 text-center animate" data-animation="scaleAppear" data-delay="120">
 	                	<h4 className="mb-3">Thanks for reaching out!</h4>
 	                	<h5 className="mt-3">Successfully submitted the form.</h5>
 			            <p>A <strong>SMAC Instructor</strong> will be in touch very soon.</p>
